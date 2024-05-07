20fca5ef4302c93a781ef710d9efc511420369cd
unknown <ameriki2012@gmail.com>
2024-05-07 09:42:43 +0100

    feat: dashboard ui

---
+++ src/app/components/dashboard/sidebar/index.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import Link from "next/link"
+
+const SideBar = () => {
+    const { loading, user, error: apiError } = useSelector(
+        (state) => state.auth
+      )
+    return (
+        <nav className="sidebar sidebar-offcanvas" id="sidebar">
+          <ul className="nav">
+            <li className="nav-item nav-profile">
+              <a href="#" className="nav-link">
+                <div className="nav-profile-image">
+                <span className="font-weight-bold mb-2">{user?.given_name?.toUpperCase()}</span><br />
+                  <span className="text-secondary text-small">{user?.email}</span>
+                  {/* <img src="assets/images/faces/face1.jpg" alt="profile" /> */}
+                  <span className="login-status online" />
+                  {/*change to offline or busy as needed*/}
+                </div>
+                <div className="nav-profile-text d-flex flex-column">
+                
+                </div>
+                {/* <i className="mdi mdi-bookmark-check text-success nav-profile-badge" /> */}
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/dashboard">
+              
+                <span className="menu-title">Dashboard</span>
+                <i className="mdi mdi-home menu-icon" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <span className="menu-title">File Store</span>
+                <i className="menu-arrow" />
+                <i className="fa fa-folder-o" />
+                
+               
+              </a>
+              <div className="collapse" id="ui-basic">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/buttons.html">Files</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/dropdowns.html">Permissions</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/typography.html">Recycle Bin</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+                <span className="menu-title">Icons</span>
+                <i className="mdi mdi-contacts menu-icon" />
+              </a>
+              <div className="collapse" id="icons">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/icons/font-awesome.html">Font Awesome</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#forms" aria-expanded="false" aria-controls="forms">
+                <span className="menu-title">Forms</span>
+                <i className="mdi mdi-format-list-bulleted menu-icon" />
+              </a>
+              <div className="collapse" id="forms">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/forms/basic_elements.html">Form Elements</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+                <span className="menu-title">Charts</span>
+                <i className="mdi mdi-chart-bar menu-icon" />
+              </a>
+              <div className="collapse" id="charts">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/charts/chartjs.html">ChartJs</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+                <span className="menu-title">Tables</span>
+                <i className="mdi mdi-table-large menu-icon" />
+              </a>
+              <div className="collapse" id="tables">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/tables/basic-table.html">Basic table</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <span className="menu-title">User Pages</span>
+                <i className="menu-arrow" />
+                <i className="mdi mdi-lock menu-icon" />
+              </a>
+              <div className="collapse" id="auth">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/login.html"> Login </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/register.html"> Register </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/error-404.html"> 404 </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/error-500.html"> 500 </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="docs/documentation.html" target="_blank">
+                <span className="menu-title">Documentation</span>
+                <i className="mdi mdi-file-document-box menu-icon" />
+              </a>
+            </li> */}
+          </ul>
+        </nav>
+    )
+}
+
+export default SideBar
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,6 +5,9 @@ import Link from "next/link"
 import { useRouter  } from 'next/navigation'
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from "../../redux/reducers/authSlice"
+import Header from '../components/dashboard/header';
+import SideBar from '../components/dashboard/sidebar';
+import Footer from '../components/dashboard/footer';
 
 const Dashboard = () => {
     const { loading, user, error: apiError } = useSelector(
@@ -33,26 +36,63 @@ const handleLogout = async () => {
     router.push('/login');
   }
   return (
-    <div className="container forgot-container">
-      <div> <button onClick={handleLogout} className="btn btn-white">Logout</button></div>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-header">
-              <h3 className="text-center">Dashboard</h3>
+  
+    
+          <><div className="container-scroller">
+
+      {/* partial:partials/_navbar.html */}
+     <Header />
+      {/* partial */}
+      <div className="container-fluid page-body-wrapper">
+        {/* partial:partials/_sidebar.html */}
+        <SideBar />
+        {/* partial */}
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <div className="page-header">
+              <h3 className="page-title">
+                <span className="page-title-icon bg-gradient-primary text-white me-2">
+                  <i className="mdi mdi-home" />
+                </span> Dashboard
+              </h3>
+              <nav aria-label="breadcrumb">
+                <ul className="breadcrumb">
+                  <li className="breadcrumb-item active" aria-current="page">
+                    <span />Overview <i className="mdi mdi-alert-circle-outline icon-sm text-primary align-middle" />
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <div className="card-body">
-             <p>email: {user?.email}</p>
-             <p>Username: {user?.username}</p>
-             <p>sub: {user?.subUser}</p>
-             <p>given_name: {user?.given_name}</p>
-             <p>family_name: {user?.family_name}</p>
-             <p>exp: {user?.exp}</p>
-            </div>
+               
           </div>
+          {/* content-wrapper ends */}
+          {/* partial:partials/_footer.html */}
+          <Footer />
+          {/* partial */}
         </div>
       </div>
-    </div>
+
+    </div><script src="assets/vendors/js/vendor.bundle.base.js" /><script src="assets/vendors/chart.js/chart.umd.js"></script><script src="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js" /><script src="assets/js/off-canvas.js" /><script src="assets/js/misc.js" /><script src="assets/js/settings.js" /><script src="assets/js/todolist.js" /><script src="assets/js/jquery.cookie.js" /><script src="assets/js/dashboard.js" /></>
+    // <div className="container forgot-container">
+    //   <div> <button onClick={handleLogout} className="btn btn-white">Logout</button></div>
+    //   <div className="row justify-content-center">
+    //     <div className="col-md-6">
+    //       <div className="card">
+    //         <div className="card-header">
+    //           <h3 className="text-center">Dashboard</h3>
+    //         </div>
+    //         <div className="card-body">
+    //          <p>email: {user?.email}</p>
+    //          <p>Username: {user?.username}</p>
+    //          <p>sub: {user?.subUser}</p>
+    //          <p>given_name: {user?.given_name}</p>
+    //          <p>family_name: {user?.family_name}</p>
+    //          <p>exp: {user?.exp}</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
