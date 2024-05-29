a703e7535cff27eff589be47f42c90ef6401372a
Aditya Tiwari <aditya.ace@hotmail.com>
2024-05-30 00:43:53 +0530

    UI bug fix

--- src/app/plansAndPrices/page.jsx
+++ src/app/plansAndPrices/page.jsx
@@ -28,15 +28,12 @@ const PlansAndPrices = () => {
   const [selectedPlan, setSelectedPlan] = useState(0);
 
   useEffect(() => {
-    if (pricePlans?.length === 0 && !loading) {
-      dispatch(getPricePlans());
-    }
-    if (subscriptions?.length === 0 && !loading) {
-      dispatch(getSubscriptions());
-    }
-  }, [loading, pricePlans?.length, subscriptions?.length]);
+    dispatch(getPricePlans());
+    dispatch(getSubscriptions());
+  }, [dispatch, pricePlans.length, subscriptions.length]);
 
   useEffect(() => {
+    console.log("We ran");
     const currencyList = {
       usd: [3, 6, 9, 12],
       chf: [1, 4, 10],
@@ -48,7 +45,7 @@ const PlansAndPrices = () => {
       );
       setGetPriceUsdPlans(getPlans);
     }
-  }, [loading, pricePlans, selectedCurrency]);
+  }, [selectedCurrency, pricePlans, loading]);
 
   const getPlanType = (type) => {
     setPricePlansType(type);
@@ -59,85 +56,89 @@ const PlansAndPrices = () => {
     setSelectedCurrency(event.target.value);
   };
 
-  const handleContinueClick = () => {
-    console.log(getPriceUsdPlans);
-    console.log(selectedPlan);
+  const handleContinueClick = React.useCallback(() => {
     const pricePlan = getPriceUsdPlans.find((item) =>
       item?.pricePlanName.includes(selectedPlan?.subscriptionPlanName)
     );
-    // Need to find how to get values for subscriptionBillingCountry and subscriptionBillingLanguage
     const selectedSubscription = {
       subscriptionName: selectedPlan?.subscriptionPlanName,
       subscriptionSelectedPricePlan: pricePlan?.currencyID,
       subscriptionBillingCountry: selectedCurrency,
       subscriptionBillingLanguage: "en",
       comment: pricePlan?.comment,
-      subscriptionBillingEmail: "", //Get it when user is logged in
+      subscriptionBillingEmail: "", // Get it when user is logged in
       subscriptionBillingPreference: pricePlansType,
       enterpriseBillingRequestEmail: "?",
     };
-    console.log(selectedSubscription);
     dispatch(createSubscription(selectedSubscription));
-  };
+  }, [
+    getPriceUsdPlans,
+    selectedPlan,
+    selectedCurrency,
+    pricePlansType,
+    dispatch,
+  ]);
 
   return (
     <div className="price-plans">
       <h1>Subscribe</h1>
-      <div className="row">
-        <span className="subscription-cta">
-          <button
-            className="btn btn-info"
-            onClick={() => {
-              getPlanType("monthly");
-            }}
-          >
-            Monthly
-          </button>
-          <button
-            className="btn btn-info"
-            onClick={() => {
-              getPlanType("quarterly");
-            }}
-          >
-            Quarterly
-          </button>
-          <button
-            className="btn btn-info"
-            onClick={() => {
-              getPlanType("yearly");
-            }}
-          >
-            Yearly
-          </button>
-        </span>
-        <div className="currency-selector-area">
-          <select
-            className="my-select selectpicker currency-selector"
-            data-container="body"
-            onChange={(e) => changeCurrency(e)}
-          >
-            <option defaultChecked value="usd">{`USD (US Dollar)`}</option>
-            <option value="chf">{`CHF (Swiss Franc)`}</option>
-            <option value="euro">{`EUR (Euro)`}</option>
-          </select>
-        </div>
-      </div>
       {!loading && getPricePlans?.length && subscriptions?.length ? (
-        <CardSlider
-          cards={getPriceUsdPlans}
-          subscriptionsCard={subscriptions}
-          subsType={pricePlansType}
-          selectedCurrency={selectedCurrency}
-          setSelectedPlan={setSelectedPlan}
-        />
+        <>
+          <div className="row">
+            <span className="subscription-cta">
+              <button
+                className="btn btn-info"
+                onClick={() => {
+                  getPlanType("monthly");
+                }}
+              >
+                Monthly
+              </button>
+              <button
+                className="btn btn-info"
+                onClick={() => {
+                  getPlanType("quarterly");
+                }}
+              >
+                Quarterly
+              </button>
+              <button
+                className="btn btn-info"
+                onClick={() => {
+                  getPlanType("yearly");
+                }}
+              >
+                Yearly
+              </button>
+            </span>
+            <div className="currency-selector-area">
+              <select
+                className="my-select selectpicker currency-selector"
+                data-container="body"
+                onChange={(e) => changeCurrency(e)}
+              >
+                <option defaultChecked value="usd">{`USD (US Dollar)`}</option>
+                <option value="chf">{`CHF (Swiss Franc)`}</option>
+                <option value="euro">{`EUR (Euro)`}</option>
+              </select>
+            </div>
+          </div>
+          <CardSlider
+            cards={getPriceUsdPlans}
+            subscriptionsCard={subscriptions}
+            subsType={pricePlansType}
+            selectedCurrency={selectedCurrency}
+            setSelectedPlan={setSelectedPlan}
+          />
+          <div className="continue-cta">
+            <button className="btn btn-primary" onClick={handleContinueClick}>
+              Continue
+            </button>
+          </div>
+        </>
       ) : (
         <Spinner label="Loading..." />
       )}
-      <div className="continue-cta">
-        <button className="btn btn-primary" onClick={handleContinueClick}>
-          Continue
-        </button>
-      </div>
     </div>
   );
 };
